1e909abd27ec60ae3a04d93702b710daa195314e
TheRedstoneRadiant <76220359+TheRedstoneRadiant@users.noreply.github.com>
2023-08-13 00:17:13 -0400

    Add wildfires button

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
-"use client";
+"use client"
 
-import React, { useEffect, useState } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
-import requestLocationPermission from "@/app/location/requestLocationPermission";
+import React, { useEffect, useState } from 'react';
+import { Loader } from '@googlemaps/js-api-loader';
+import requestLocationPermission from '@/app/location/requestLocationPermission';
 
 const Map = () => {
   const [showWildfires, setShowWildfires] = useState(false);
@@ -12,12 +12,12 @@ const Map = () => {
   useEffect(() => {
     const loader = new Loader({
       apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-      version: "weekly",
-      libraries: ["places"],
+      version: 'weekly',
+      libraries: ['places'],
     });
 
     loader.load().then(() => {
-      const initialMap = new google.maps.Map(document.getElementById("map"), {
+      const initialMap = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 0, lng: 0 },
         zoom: 4,
         mapTypeControl: false,
@@ -25,9 +25,9 @@ const Map = () => {
         fullscreenControl: false,
       });
 
-      fetch("https://ipapi.co/json/")
-        .then((response) => response.json())
-        .then((data) => {
+      fetch('https://ipapi.co/json/')
+        .then(response => response.json())
+        .then(data => {
           const approxLocation = {
             lat: parseFloat(data.latitude),
             lng: parseFloat(data.longitude),
@@ -37,7 +37,7 @@ const Map = () => {
           setMap(initialMap);
         })
         .catch(() => {
-          console.error("Error: Failed to approximate location using IP.");
+          console.error('Error: Failed to approximate location using IP.');
           requestLocationPermission(initialMap);
         });
     });
@@ -51,43 +51,41 @@ const Map = () => {
 
       fetch(url, {
         headers: {
-          "x-api-key": process.env.NEXT_PUBLIC_AMBEE_API_KEY,
-          "Content-type": "application/json",
+          'x-api-key': process.env.NEXT_PUBLIC_AMBEE_API_KEY,
+          'Content-type': 'application/json',
         },
       })
-        .then((response) => {
+        .then(response => {
           if (!response.ok) {
-            throw new Error("Network response was not ok");
+            throw new Error('Network response was not ok');
           }
           return response.json();
         })
-        .then((data) => {
-          if (data.message === "No data available." || data.data.length === 0) {
-            console.log("No wildfire data available.");
+        .then(data => {
+          if (data.message === 'No data available.' || data.data.length === 0) {
+            console.log('No wildfire data available.');
           } else {
             setWildfireData(data.result);
           }
         })
-        .catch((error) => {
-          console.error("Error fetching wildfire data:", error);
+        .catch(error => {
+          console.error('Error fetching wildfire data:', error);
         });
     }
   }, [showWildfires, map, wildfireData]);
 
   useEffect(() => {
     if (showWildfires && map && wildfireData.length) {
-      // Clear existing markers
-      const markers = wildfireData.map((wildfire) => {
+      const markers = wildfireData.map(wildfire => {
         const marker = new google.maps.Marker({
           position: { lat: wildfire.lat, lng: wildfire.lng },
           map,
           icon: {
-            url: "https://maps.google.com/mapfiles/ms/icons/orange-dot.png",
+            url: 'https://maps.google.com/mapfiles/ms/icons/orange-dot.png',
           },
         });
 
-        marker.addListener("click", () => {
-          // Create popup content
+        marker.addListener('click', () => {
           const content = `
             <div>
               <h3>Wildfire Information</h3>
@@ -97,7 +95,6 @@ const Map = () => {
             </div>
           `;
 
-          // Create popup
           const infowindow = new google.maps.InfoWindow({
             content,
           });
@@ -109,20 +106,18 @@ const Map = () => {
       });
 
       return () => {
-        // Remove markers when toggling off
-        markers.forEach((marker) => marker.setMap(null));
+        markers.forEach(marker => marker.setMap(null));
       };
     }
   }, [showWildfires, map, wildfireData]);
 
   return (
     <main>
-      <div id="map" style={{ width: "100%", height: "100vh" }}></div>
+      <div id="map" style={{ width: '100%', height: '100vh' }}></div>
       <button
         onClick={() => setShowWildfires(!showWildfires)}
-        className={`fixed top-4 right-4 ${
-          showWildfires ? "bg-orange-500" : "bg-gray-500"
-        } text-white px-4 py-2 rounded-md`}
+        className={`fixed top-4 right-4 ${showWildfires ? 'bg-orange-500' : 'bg-gray-500'
+          } text-white px-4 py-2 rounded-md`}
       >
         Wildfires
       </button>
